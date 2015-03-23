976c5478acf430bfa15c79999033dc23cdc9d135
Galder Zamarreño <galder@zamarreno.com>
2015-03-23 17:22:03 +0100

    Add y/n and theme for Emacs Basic

--- emacs.d/basic-init.el
+++ emacs.d/basic-init.el
@@ -2,3 +2,12 @@
 
 ;; Enable numbered columns by default
 (setq column-number-mode t)
+
+;; Always ask for y/n keypress instead of typing out 'yes' or 'no'
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Load 'wombat' theme only in terminal mode
+(defun on-after-init ()
+  (unless (display-graphic-p (selected-frame))
+    (load-theme 'wombat)))
+(add-hook 'window-setup-hook 'on-after-init)
