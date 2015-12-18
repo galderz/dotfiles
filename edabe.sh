c821710806c46f6ab7c5c084f85b7804a3929284
Galder Zamarreño <galder@zamarreno.com>
2015-12-18 12:42:57 +0100

    Magic potion created

---
+++ edabe.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+brew install --with-cocoa --srgb emacs
+brew linkapps emacs
+brew cask install java
+brew install maven
+brew install git
+brew install wget
+brew install highlight
+brew install nvm
+brew install unrar
+brew install magit
+
+brew cask install iterm2
+brew cask install caskroom/versions/java7
+brew cask install intellij-idea
+brew cask install skype
+brew cask install notational-velocity
+brew cask install dropbox
+brew cask install 1password
+brew cask install alfred
+brew cask install mailplane
+brew cask install google-chrome
+brew cask install firefox
+brew cask install istat-menus
+brew cask install vlc
