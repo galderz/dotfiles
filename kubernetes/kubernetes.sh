3b3646d04fbbfdbe7ad5d763b4536ebd3d8686ec
Galder Zamarreño <galder@zamarreno.com>
2017-10-15 16:11:01 +0200

    Add Kompose to OpenShift path

--- kubernetes/kubernetes.sh
+++ kubernetes/kubernetes.sh
@@ -2,14 +2,18 @@
 
 
 export KUBETAIL_HOME="/opt/kubetail"
-export PATH=$KUBECTL_HOME:$KUBETAIL_HOME:$PATH
+export KOMPOSE_HOME="/opt/kompose"
+export PATH=$KUBECTL_HOME:$KUBETAIL_HOME:$KOMPOSE_HOME:$PATH
 
 echo `docker --version`
 
 KUBECTL_VERSION=`kubectl version --short=true --client=true`
-KUBECTL_VERSION=$(printf "kubectl %s" "$KUBECTL_VERSION")
+KUBECTL_VERSION=$(printf "\nkubectl %s" "$KUBECTL_VERSION")
 echo $KUBECTL_VERSION
 
 
 KUBETAIL_VERSION=$(printf "kubetail v%s" "$(kubetail --version)")
 echo $KUBETAIL_VERSION
+
+KOMPOSE_VERSION=$(printf "kompose v%s" "$(kompose version)")
+echo $KOMPOSE_VERSION
