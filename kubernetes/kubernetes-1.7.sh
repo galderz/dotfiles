caf10825ab8422b763a5be3d8fc4813aa11d2ed1
Galder Zamarreño <galder@zamarreno.com>
2017-11-15 16:24:10 +0100

    Add OpenShift 3.7 scripts

---
+++ kubernetes/kubernetes-1.7.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+export KUBECTL_HOME="/opt/kubectl-1.7"
+
+
+source ~/.dotfiles/kubernetes/kubernetes.sh
---
+++ openshift/openshift-3.7.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+source ~/.dotfiles/kubernetes/kubernetes-1.7.sh
+
+
+export OPENSHIFT_CLIENT_HOME="/opt/openshift-client-3.7"
+source ~/.dotfiles/openshift/openshift.sh
