5e0d5e4f7c1b9876560120507857641ef6ffce12
Galder Zamarreño <galder@zamarreno.com>
2018-01-10 16:22:03 +0100

    Add minishift aliases

---
+++ openshift/minishift.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+export MINISHIFT_HOME="/opt/minishift"
+export PATH=$MINISHIFT_HOME:$PATH
+
+
+MINISHIFT_VERSION=`minishift version`
+MINISHIFT_VERSION=$(printf "%s", "$MINISHIFT_VERSION")
+echo $MINISHIFT_VERSION
