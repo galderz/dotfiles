172460768594ddf90e52b27d17ead58a0fc734ce
Galder Zamarreño <galder@zamarreno.com>
2018-01-18 15:22:26 +0100

    Move k8s-cleanup to a separate bin directory and add it to PATH

--- kubernetes/minikube.sh
+++ kubernetes/minikube.sh
@@ -3,7 +3,8 @@
 
 export MINIKUBE_HOME="/opt/minikube"
 export KUBECTL_HOME=""
-export PATH=$MINIKUBE_HOME:$PATH
+export KUBE_BIN_HOME="/Users/g/0/dotfiles/kubernetes/bin"
+export PATH=$KUBE_BIN_HOME:$MINIKUBE_HOME:$PATH
 
 echo `minikube version`
 #echo `kubectl version`
