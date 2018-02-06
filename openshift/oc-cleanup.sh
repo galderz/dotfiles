dba0c3d6667ee12a617b3f41e3f1fa47181dedce
Galder Zamarreño <galder@zamarreno.com>
2018-02-06 23:28:16 +0100

    Use simpler cleanup script for OpenShift

--- openshift/oc-cleanup.sh
+++ openshift/oc-cleanup.sh
@@ -1,33 +1,37 @@
 #!/usr/bin/env bash
 PROJECT=$(oc project -q)
 echo "Cleaning up current project: ${PROJECT}"
+
+oc delete all --all
+
 # https://docs.openshift.org/latest/cli_reference/basic_cli_operations.html#object-types
-oc delete build --all
-oc delete bc --all
+# oc delete build --all
+# oc delete bc --all
 
-oc delete dc --all
-oc delete deploy --all
+# oc delete dc --all
+# oc delete deploy --all
 
-oc delete is --all
-oc delete istag --all
-oc delete isimage --all
+# oc delete is --all
+# oc delete istag --all
+# oc delete isimage --all
 
-oc delete job --all
+# oc delete job --all
 
-oc delete po --all
-oc delete rc --all
-oc delete rs --all
+# oc delete po --all
+# oc delete rc --all
+# oc delete rs --all
 
-oc delete secrets --all
-oc delete configmap --all
+# oc delete secrets --all
+# oc delete configmap --all
 
-oc delete services --all
+# oc delete services --all
 
-oc delete routes --all
+# oc delete routes --all
 
-oc delete template --all
+# oc delete template --all
 
-oc delete serviceaccounts --all
-oc delete rolebindings --all
+### Never use!
+#oc delete rolebindings --all
+#oc delete serviceaccounts --all
 
-oc delete statefulsets --all
+# oc delete statefulsets --all
