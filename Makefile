07ad9042904cabaf5b1ecdd0d839aa14a121def8
Galder Zamarreño <galder@zamarreno.com>
2019-03-13 18:54:54 +0100

    Add makefile for init

---
+++ Makefile
@@ -0,0 +1,71 @@
+.PHONY: dep build image push run clean help
+
+
+.DEFAULT_GOAL := help
+
+
+dotfiles:
+	git remote rm origin
+	git remote add origin git@github.com:galderz/dotfiles.git
+
+
+opt:
+	sudo mkdir /opt
+	cd /opt
+	sudo chwon $(whoami) .
+
+
+ansible:
+	sudo easy_install pip
+	sudo pip install "ansible"
+
+
+playbook:
+	mkdir ~/1/macos
+	cd ~/1/macos
+	git clone https://github.com/galderz/mac-dev-playbook
+	cd mac-dev-playbook
+	git remote rm origin
+	git remote add origin git@github.com:galderz/mac-dev-playbook.git
+	git remote add upstream https://github.com/geerlingguy/mac-dev-playbook
+	ansible-galaxy install -r requirements.yml
+	ansible-playbook main.yml -i inventory -K
+
+
+emacs:
+	cd ~
+	git clone https://github.com/galderz/prelude .emacs.d
+	cd .emacs.d
+	git remote rm origin
+	git remote add origin git@github.com:galderz/prelude.git
+
+
+symlinks:
+	cd ~
+	ln -s ~/1/dotfiles .dotfiles
+	ln -s ~/.dotfiles/git/gitconfig .gitconfig
+	ln -s ~/.dotfiles/log.d .log.d
+
+
+macos:
+	cd ~/1/dotfiles/macos
+	./mathias-macos
+	./my-macos
+
+
+# Install emacs separately:
+# - pkgconfig: pre-requirement for emacs
+# - Ignore the warning saying to install the cask version,
+#   because it does not work as well in terms of window management install
+brew:
+	sudo chown -R "$(whoami)" /usr/local/lib/pkgconfig
+	brew install --with-cocoa emacs
+	brew install homebrew/emacs/magit
+
+
+virtualbox:
+	brew cask install virtualbox
+
+
+## all:             Execute all tutorial steps
+all: dotfiles opt ansible playbook emacs symlinks macos brew virtualbox
--- init.sh
+++ init.sh
@@ -10,6 +10,14 @@ dotfiles() {
 }
 
 
+permissions() {
+  # Make /opt
+  sudo mkdir /opt
+  cd /opt
+  sudo chwon "$(whoami)" .
+}
+
+
 ansible() {
   sudo easy_install pip
   sudo pip install "ansible"
@@ -17,8 +25,8 @@ ansible() {
 
 
 playbook() {
-  mkdir -p ~/1/macos
-  cd !$
+  mkdir ~/1/macos
+  cd ~/1/macos
   git clone https://github.com/galderz/mac-dev-playbook
   cd mac-dev-playbook
   git remote rm origin
@@ -30,20 +38,58 @@ playbook() {
 
 
 emacs() {
-    cd ~
-    git clone https://github.com/galderz/prelude .emacs.d
-    cd .emacs.d
-    git remote rm origin
-    git remote add origin git@github.com:galderz/prelude.git
+  cd ~
+  git clone https://github.com/galderz/prelude .emacs.d
+  cd .emacs.d
+  git remote rm origin
+  git remote add origin git@github.com:galderz/prelude.git
+}
+
+
+symlinks() {
+  cd ~
+  ln -s ~/1/dotfiles .dotfiles
+  ln -s ~/.dotfiles/git/gitconfig .gitconfig
+  ln -s ~/.dotfiles/log.d .log.d
+}
+
+
+macos() {
+  cd ~/1/dotfiles/macos
+  ./mathias-macos
+  ./my-macos
+}
+
+
+brew() {
+  # Pre-requirement for emacs
+  sudo chown -R "$(whoami)" /usr/local/lib/pkgconfig
+
+  # Install emacs separately.
+  # Ignore the warning saying to install the cask version,
+  # because it does not work as well in terms of window management
+  brew install --with-cocoa emacs
+  brew install homebrew/emacs/magit
+
+  # Installed last and outside Ansible due to issues with software provider
+  # brew cask install virtualbox
 }
 
 
 main() {
   dotfiles
+  permissions
   ansible
   playbook
-  # TODO move to ansible
   emacs
+  symlinks
+  ssh
+  # TODO transfer files
+  # TODO install fonts
+  brew
+  macos
+
+  echo "Next, install virtualbox manually"
 }
 
 
