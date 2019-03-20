3396c52d432753bcd625ab76a28c8c7758ce2dec
Galder Zamarreño <galder@zamarreno.com>
2019-03-20 19:29:58 +0100

    Another go at fixing directory names

--- init/iterm2.sh
+++ init/iterm2.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-ITERM2_PROFILE_DIR="~/Library/Application\ Support/iTerm2/DynamicProfiles"
+ITERM2_PROFILE_DIR="~/Library/Application Support/iTerm2/DynamicProfiles"
 
-mkdir -p ${ITERM2_PROFILE_DIR}
-cp ~/.dotfiles/iterm2/my-profile.json ${ITERM2_PROFILE_DIR}
+mkdir -p "${ITERM2_PROFILE_DIR}"
+cp ~/.dotfiles/iterm2/my-profile.json "${ITERM2_PROFILE_DIR}"
