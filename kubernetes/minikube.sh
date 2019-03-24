fe5b73003f14b20e204e3912d3c51b86018ce57b
Galder Zamarreño <galder@zamarreno.com>
2019-03-24 12:23:35 +0100

    Add Minikube configuration and correct paths

--- kubernetes/minikube.sh
+++ kubernetes/minikube.sh
@@ -4,11 +4,11 @@
 export MINIKUBE_HOME="/opt/minikube"
 export KUBECTL_HOME="/opt/minikube"
 export HELM_HOME="/opt/helm"
-export KUBE_BIN_HOME="/Users/g/0/dotfiles/kubernetes/bin"
-export PATH=$KUBE_BIN_HOME:$MINIKUBE_HOME:$HELM_HOME:$PATH
+export KUBE_BIN_HOME="${HOME}/.dotfiles/kubernetes/bin"
+export PATH=${KUBE_BIN_HOME}:${MINIKUBE_HOME}:${HELM_HOME}:$PATH
 
 alias oc="kubectl"
 
 echo `minikube version`
-echo `kubectl version`
-echo `helm version`
+#echo `kubectl version`
+#echo `helm version`
