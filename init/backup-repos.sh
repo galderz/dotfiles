a057c80ede32caf6b52b2d4383a6fa5e815f0504
Galder Zamarreño <galder@zamarreno.com>
2019-03-25 10:02:44 +0100

    Add scripts to backup/restore github repos

---
+++ init/backup-repos.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+BACKUP=/Volumes/Backup/Backup
+
+cp -r ~/1 ${BACKUP}
+cp -r ~/2 ${BACKUP}
+cp -r ~/3 ${BACKUP}
+
---
+++ init/restore-repos.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+BACKUP=/Volumes/Backup/Backup
+
+cp -r ${BACKUP}/1 ${BACKUP}
+cp -r ${BACKUP}/2 ${BACKUP}
+cp -r ${BACKUP}/3 ${BACKUP}
+
