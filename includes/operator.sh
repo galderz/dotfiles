87d167898dbb78909305f831a169a4379930f984
Galder Zamarreño <galder@zamarreno.com>
2019-11-08 15:37:10 +0100

    Update operator include, no longer need docker-machine

--- includes/operator.sh
+++ includes/operator.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-
-source ~/.dotfiles/docker/docker-machine.sh
 source ~/.dotfiles/go/golang.sh
-source ~/.dotfiles/openshift/openshift-4.0.sh
+source ~/.dotfiles/openshift/openshift-4.1.sh
 
-
-# Change to operator directory
 pushd $GOPATH/src/github.com/infinispan/infinispan-operator
 git status
-
-
-# Set docker machine environment variables
-# If docker-machine not yet started it won't have effect
-# If started it will be applied
-eval "$(docker-machine env default)"
