40810cdfdbd2b2276f39b453939b6a582f081e02
Galder Zamarreño <galder@redhat.com>
2020-10-29 17:34:08 +0100

    Do not call git status

--- includes/mandrel-mandrel-dev.sh
+++ includes/mandrel-mandrel-dev.sh
@@ -8,5 +8,5 @@ source ~/.dotfiles/java/mx.sh
 alias graal-style="mx eclipseformat -e /Applications/Eclipse\ Java.app/Contents/MacOS/eclipse --primary && mx checkstyle"
 
 pushd ~/1/mandrel-mandrel/substratevm
-git status
+# git status
 
