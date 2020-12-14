07dc0d79b0d4d8054b62ec58121e6547f7e4a9b9
Galder Zamarreño <galder@redhat.com>
2020-12-14 11:49:40 +0100

    Use install folder instead

--- jbang/jbanging.sh
+++ jbang/jbanging.sh
@@ -1,4 +1,4 @@
-export JBANG_HOME="${HOME}/1/jbang/build/distributions/jbang"
+export JBANG_HOME="${HOME}/1/jbang/build/install/jbang"
 export JBANG_BINDIR=$JBANG_HOME"/bin"
 
 export PATH=$JBANG_BINDIR:$PATH
