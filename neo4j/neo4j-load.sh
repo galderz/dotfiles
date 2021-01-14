1e1d148a6b357f2d6bc8ac4cf0832f2260515173
Galder Zamarreño <galder@redhat.com>
2021-01-14 12:09:46 +0100

    Fix variable wrapping

--- neo4j/neo4j-load.sh
+++ neo4j/neo4j-load.sh
@@ -7,9 +7,9 @@ NEO_HOME=/opt/neo4j
 neoshell="${NEO_HOME}/bin/cypher-shell -u neo4j -p neo"
 
 rm -f ${NEO_HOME}/import/*
-$(neoshell) "MATCH(n) DETACH DELETE n" || true
-$(neoshell) "DROP CONSTRAINT unique_vm_id" || true
-$(neoshell) "DROP CONSTRAINT unique_method_id" || true
+${neoshell} "MATCH(n) DETACH DELETE n" || true
+${neoshell} "DROP CONSTRAINT unique_vm_id" || true
+${neoshell} "DROP CONSTRAINT unique_method_id" || true
 
 cp $(REPORTS_DIR)/*.csv ${NEO_HOME}/import
-time cat $(shell find $(REPORTS_DIR) -name '*.cypher') | $(neoshell)
+time cat $(shell find $(REPORTS_DIR) -name '*.cypher') | ${neoshell}
