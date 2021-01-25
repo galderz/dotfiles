da8d2f72acd7a7859b441a79bd9d964b28e00f40
Galder Zamarreño <galder@redhat.com>
2021-01-25 14:28:59 +0100

    Fix paths and remove commented code

--- includes/mandrel-mandrel-dev.sh
+++ includes/mandrel-mandrel-dev.sh
@@ -5,8 +5,7 @@ source ~/.dotfiles/java/java.sh
 
 source ~/.dotfiles/java/mx.sh
 
-alias graal-style="mx eclipseformat -e /Applications/Eclipse\ Java.app/Contents/MacOS/eclipse --primary && mx checkstyle"
+alias graal-style="mx eclipseformat -e /opt/eclipse/eclipse --primary && mx checkstyle"
 
 pushd ~/1/mandrel-mandrel/substratevm
-# git status
 
