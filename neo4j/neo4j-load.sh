f439c1676fadc7393f328724198d89b3ed9bbce8
Galder Zamarreño <galder@redhat.com>
2021-01-25 14:32:02 +0100

    Tweak neo4j script

--- neo4j/neo4j-load.sh
+++ neo4j/neo4j-load.sh
@@ -11,5 +11,5 @@ ${neoshell} "MATCH(n) DETACH DELETE n" || true
 ${neoshell} "DROP CONSTRAINT unique_vm_id" || true
 ${neoshell} "DROP CONSTRAINT unique_method_id" || true
 
-cp $(REPORTS_DIR)/*.csv ${NEO_HOME}/import
-time cat $(shell find $(REPORTS_DIR) -name '*.cypher') | ${neoshell}
+cp ${REPORTS_DIR}/*.csv ${NEO_HOME}/import
+time cat $(find ${REPORTS_DIR} -name '*.cypher') | ${neoshell}
